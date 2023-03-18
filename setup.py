5c02ec26a4e55a642651318e60dfdc91813db737
Logan Reece <onereddime@protonmail.com>
2023-03-18 14:35:19 -0500

    2.0.2
    Installation instructions

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if 'DEBBUILD' not in os.environ:
 
 setup(
     name='daily-cli',
-    version='2.0.1',
+    version='2.0.2',
     description='Command-line notebook for daily entries.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
