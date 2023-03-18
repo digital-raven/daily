d5dec2e368c4ab428c0d03337e4476f2c4f13e22
Logan Reece <onereddime@protonmail.com>
2023-03-18 14:35:19 -0500

    breaking: Changed "upcoming" to "todo"

--- daily/cli/todo/parser.py
+++ daily/cli/todo/parser.py
@@ -6,8 +6,8 @@ filter_opts = create_filter_opts()
 
 def add_subparser(subparsers):
     sp = subparsers.add_parser(
-        'upcoming',
-        help='Display upcoming events.',
+        'todo',
+        help='Display todos.',
         description=(
             'These are any entries with an "events" heading within the'
             '(default) range of 2 weeks.'),
